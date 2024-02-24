641ea8394d50673fa5d46f4d9e7680a7ada54d9b
Atharva Chaudhari <92793651+Atharva-C@users.noreply.github.com>
2024-02-24 14:28:33 +0000

    >>

--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,25 @@
-console.log("Atharva")
+// console.log("Atharva")
+//strings
+
+const name = "Atharva"
+const age = 21
+
+//string interpolation
+// console.log(`hello my name is ${name} I am ${age} years old`);
+
+const game = new String('mario');
+// console.log(game);
+// console.log(game[0]);
+
+// console.log(game.toUpperCase());
+// console.log(game.charAt(2));
+// console.log(game.indexOf('i'));
+
+//===========================================
+console.log(Math.random());//gives value b/w 0 and 1
+console.log((Math.random()*10) + 1);
+
+const min = 100
+const max = 1000
+
+console.log(Math.floor(Math.random()*(max - min + 1) + min));
