3dfdbec9ca8add85db6c8f8a69ecec5bfc3ddf6c
Atharva Chaudhari <92793651+Atharva-C@users.noreply.github.com>
2024-02-25 14:25:54 +0000

    Date sort

--- basics/test.js
+++ basics/test.js
@@ -16,10 +16,26 @@ const game = new String('mario');
 // console.log(game.indexOf('i'));
 
 //===========================================
-console.log(Math.random());//gives value b/w 0 and 1
-console.log((Math.random()*10) + 1);
+// console.log(Math.random());//gives value b/w 0 and 1
+// console.log((Math.random()*10) + 1);
 
 const min = 100
 const max = 1000
 
-console.log(Math.floor(Math.random()*(max - min + 1) + min));
+// console.log(Math.floor(Math.random()*(max - min + 1) + min));
+
+//-------------------------------------------------------------
+const myDate = new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toISOString());
+// console.log(typeof (myDate));
+
+const myBday = new Date(1975, 6, 14)
+console.log(myBday.toDateString());
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
+console.log(myBday.getTime());
