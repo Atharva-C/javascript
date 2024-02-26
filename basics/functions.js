1f27610338d91610f044351f8fd48fce63082d55
Atharva Chaudhari <92793651+Atharva-C@users.noreply.github.com>
2024-02-26 10:26:57 +0000

    functions started

---
+++ basics/functions.js
@@ -0,0 +1,15 @@
+// function addTwoNumbers(num1, num2){ //herenum1 and num2 are known as parameters
+//     console.log(num1 + num2);
+// }
+
+// const result = addTwoNumbers(3,4) // here 3 and 4 are known as arguements.
+// // console.log(result);
+
+
+function addTwoNumbers(num1, num2){
+    let result = num1 + num2
+    return result
+}
+
+const result = addTwoNumbers(3,4)
+console.log(result);
