c2f4bd38a88d651210547d71009e42f81992ef60
Atharva Chaudhari <92793651+Atharva-C@users.noreply.github.com>
2024-02-26 06:45:21 +0000

    done till arrays

--- basics/test.js
+++ basics/test.js
@@ -1,13 +1,13 @@
 // console.log("Atharva")
 //strings
 
-const name = "Atharva"
-const age = 21
+// const name = "Atharva"
+// const age = 21
 
 //string interpolation
 // console.log(`hello my name is ${name} I am ${age} years old`);
 
-const game = new String('mario');
+// const game = new String('mario');
 // console.log(game);
 // console.log(game[0]);
 
@@ -19,13 +19,13 @@ const game = new String('mario');
 // console.log(Math.random());//gives value b/w 0 and 1
 // console.log((Math.random()*10) + 1);
 
-const min = 100
-const max = 1000
+// const min = 100
+// const max = 1000
 
 // console.log(Math.floor(Math.random()*(max - min + 1) + min));
 
 //-------------------------------------------------------------
-const myDate = new Date();
+// const myDate = new Date();
 // console.log(myDate.toString());
 // console.log(myDate.toDateString());
 // console.log(myDate.toLocaleString());
@@ -33,9 +33,34 @@ const myDate = new Date();
 // console.log(myDate.toISOString());
 // console.log(typeof (myDate));
 
-const myBday = new Date(1975, 6, 14)
-console.log(myBday.toDateString());
+// const myBday = new Date(1975, 6, 14)
+// console.log(myBday.toDateString());
 
-let myTimeStamp = Date.now()
-console.log(myTimeStamp);
-console.log(myBday.getTime());
+// let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myBday.getTime());
+
+//arrays-------------------------------------------
+//In Js array can contain a mix of different datatypes and are also resizable.
+// const myArray = [0,1,2,3,4,5]
+// console.log(myArray[3]);
+
+// const arr2 = new Array(1,2,3);
+// console.log(arr2);
+
+// myArray.unshift(9) //insertion at the beginning.
+// console.log(myArray);
+// myArray.shift() //shifts array by one element.
+// console.log(myArray);
+
+//splice changes the original array while slice does not.
+// console.log("A", myArray);
+
+// const myn1 = myArray.slice(1,3)
+// console.log(myn1);
+// console.log("B", myArray);
+
+// const myn2 = myArray.splice(1,3)
+// console.log(myn2);
+// console.log("C", myArray);
+
